07d6ba0c87f4537cfd1520a4687919b604014841
Denis Perov <denis.perov@gmail.com>
2024-11-16 23:25:25 +0700

    Fix /pop command

--- bot/src/handler/pop-offer.js
+++ bot/src/handler/pop-offer.js
@@ -15,17 +15,18 @@ export async function handler(context) {
   //TODO: Check if sender is current owner of the chip
   if (params.chip) {
     const url = new URL(process.env.FRAME_BASE_URL);
-    const offer = await (await pbt(sender.address)).read.offers([parseInt(params.chip)]);
-    if (offer) {
+    const offers = await (await pbt(sender.address)).read.getActiveOffersByTokenId([parseInt(params.chip)]);
+    if (offers && offers.length > 0) {
+      const offer = offers[offers.length - 1];
       url.searchParams.set('action', 'popOffer');
       url.searchParams.set('chip', params.chip);
-      url.searchParams.set('amount', formatEther(offer[2]));
+      url.searchParams.set('amount', formatEther(offer.amount));
       url.searchParams.set('token', 'ETH');
       url.searchParams.set('ts', Date.now().toString());
       await context.send(url.toString());
       //HACK: Fake the smart contract call
-      // await (await pbt(sender.address)).write.acceptOffer([parseInt(params.chip, 8)]);
-      // await context.send('Psst: done 🥷');
+      await (await pbt(sender.address)).write.acceptOffer([offer.id]);
+      await context.send('Psst: done 🥷');
     } else {
     }
   } else {
