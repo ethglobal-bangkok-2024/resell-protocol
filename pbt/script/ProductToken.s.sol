1e8f2262fa32c39f96ccc4eee37aec908b5a7ce5
metalboyrick <rsulisthio@gmail.com>
2024-11-16 21:00:45 +0700

    chore: add curvegrid multibaas

--- pbt/script/ProductToken.s.sol
+++ pbt/script/ProductToken.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {ProductToken} from "../src/ProductToken.sol";
+import {MultiBaas} from "forge-multibaas/MultiBaas.sol";
 
 contract ProductTokenScript is Script {
     ProductToken public productToken;
@@ -14,6 +15,8 @@ contract ProductTokenScript is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         productToken = new ProductToken("Louis Vuitton PBT", "LVPBT");
+
+        MultiBaas.linkContract("ProductToken", address(productToken));
 
         vm.stopBroadcast();
     }
