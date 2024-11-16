a1949b157404a534cf106727defa41f16768afd9
metalboyrick <rsulisthio@gmail.com>
2024-11-16 12:11:00 +0700

    wip: add skeleton for offer features

--- pbt/src/ProductToken.sol
+++ pbt/src/ProductToken.sol
@@ -15,6 +15,23 @@ contract ProductToken is ERC721, IERC5791, Ownable {
     mapping(address => uint256) _chipAddressToTokenId;
     mapping(uint256 => address) _tokenIdToChipAddress;
 
+    struct Offer {
+        uint256 id;
+        uint256 tokenId;
+        uint256 amount;
+        address offerer;
+    }
+
+    event OfferCreated(
+        uint256 indexed offerId,
+        uint256 indexed tokenId,
+        uint256 indexed amount,
+        address offerer
+    );
+    event OfferCancelled(uint256 indexed offerId);
+    event OfferAccepted(uint256 indexed offerId);
+    event OfferRejected(uint256 indexed offerId);
+
     constructor(
         string memory name,
         string memory symbol
@@ -49,14 +66,23 @@ contract ProductToken is ERC721, IERC5791, Ownable {
         bytes32 data,
         bytes calldata signature
     ) public pure returns (address signer) {
-        bytes32 messageHash = keccak256(abi.encodePacked(data));
+        // bytes32 messageHash = bytes32(abi.encode(data));
 
-        address recoveredSigner = ECDSA.recover(
-            MessageHashUtils.toEthSignedMessageHash(messageHash),
-            signature
-        );
+        // address recoveredSigner = keccak256().recover(
+        //     MessageHashUtils.toEthSignedMessageHash(messageHash),
+        //     signature
+        // );
 
-        return recoveredSigner;
+        // // (bytes32 r, bytes32 s, uint8 v) = _splitSignature(signature);
+
+        // // address recoveredSigner = ecrecover(messageHash, v, r, s);
+
+        // return recoveredSigner;
+
+        return
+            MessageHashUtils
+                .toEthSignedMessageHash(abi.encodePacked(data))
+                .recover(signature);
     }
 
     function isChipSignatureForToken(
@@ -104,4 +130,49 @@ contract ProductToken is ERC721, IERC5791, Ownable {
         bool useSafeTransferFrom,
         bytes calldata extras
     ) external override returns (uint256 tokenId) {}
+
+    /// TODO: implement real logic
+    function createOffer(uint256 tokenId, uint256 amount) public {
+        emit OfferCreated(123, tokenId, amount, msg.sender);
+    }
+
+    /// TODO: implement real logic
+    function cancelOffer(uint256 offerId) public {
+        emit OfferCancelled(offerId);
+    }
+
+    /// TODO: implement real logic
+    function acceptOffer(uint256 offerId) public {
+        emit OfferAccepted(offerId);
+    }
+
+    /// TODO: implement real logic
+    function rejectOffer(uint256 offerId) public {
+        emit OfferRejected(offerId);
+    }
+
+    /// TODO: implement real logic
+    /// @dev first arg is offer id, second args is amount, third arg is address
+    function getOffers() public pure returns (Offer[] memory offers) {
+        Offer memory firstOffer = Offer({
+            id: 1,
+            offerer: address(0x123),
+            amount: 1000,
+            tokenId: 1
+        });
+
+        Offer memory secondOffer = Offer({
+            id: 2,
+            offerer: address(0x987),
+            amount: 1000,
+            tokenId: 2
+        });
+
+        Offer[] memory returnOffer;
+
+        returnOffer[1] = firstOffer;
+        returnOffer[2] = secondOffer;
+
+        return returnOffer;
+    }
 }
